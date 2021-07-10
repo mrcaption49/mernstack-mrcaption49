cb43d566d3bca132e5931f42d299b79859743ce0
mrcaption49 <64625737+mrcaption49@users.noreply.github.com>
2021-07-10 18:35:39 +0530

    Adding Color to Console

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     })
 
-    console.log(`Mongodb Connection:"${conn.connection.host}`)
+    console.log(`Mongodb Connection:"${conn.connection.host}`.cyan.underline.bold)
   } catch (error) {
-    console.log(`Error: ${error.message}`)
+    console.log(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
