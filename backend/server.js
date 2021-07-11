6c63cc884d355168a3b87610bbe09f2a5e9a672a
mrcaption49 <64625737+mrcaption49@users.noreply.github.com>
2021-07-12 00:44:48 +0530

    Creating Redux Store

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import colors from 'colors'
 import connectDB from './config/db.js'
 
