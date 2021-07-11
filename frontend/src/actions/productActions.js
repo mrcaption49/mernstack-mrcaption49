f02beab362941ab46bde3bb557b9bbae760fb93b
mrcaption49 <64625737+mrcaption49@users.noreply.github.com>
2021-07-12 02:09:02 +0530

    Bringing Redux State Into HOMESCREEN

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,7 +20,7 @@ export const listProducts = () => async (dispatch) => {
       type: PRODUCT_LIST_FAIL,
       payload:
         error.response && error.response.data.message
-          ? errpr.response.data.message
+          ? error.response.data.message
           : error.message,
     })
   }
